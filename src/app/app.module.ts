b3dd3032ccb104548575a1c5bb11f03cce62074c
johjan Martinez <johjanbaquerom>
2025-01-21 13:52:38 -0500

    ajuste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,10 @@ import { RouterModule } from '@angular/router';  // Importa RouterModule
 @Component({
   selector: 'app-root',
   standalone: true,  // Marca el componente como standalone
-  imports: [RouterModule],   // Asegúrate de importar RouterModule aquí
-  providers: [provideHttpClient()],
+  imports: [RouterModule, HttpClientModule],   // Asegúrate de importar RouterModule aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Mar_Paraiso';
 }
-function provideHttpClient(): import("@angular/core").Provider {
-  throw new Error('Function not implemented.');
-}
-
